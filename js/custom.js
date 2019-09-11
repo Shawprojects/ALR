5cd6df706e9aadf2b6b192f470f7b01fca941768
Shawprojects <contact.nshaw@gmail.com>
2019-09-11 15:27:40 +0530

    New Front End Techonology

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+// Tab Init
+$(document).ready(function () {
+    $('.tabs').tabs();
+});
+
+// Datepicker Init
+$(document).ready(function () {
+    $('.datepicker').datepicker();
+});
+
+// Timepicker Init
+$(document).ready(function () {
+    $('.timepicker').timepicker();
+});
+
+// Model Init
+$(document).ready(function () {
+    $('.modal').modal();
+});
